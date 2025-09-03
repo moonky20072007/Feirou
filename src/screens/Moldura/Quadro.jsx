f8b28f42a0daa9d4d3f875693aea9a09bbc4bce0
moonky_https <159582245+moonky20072007@users.noreply.github.com>
2025-09-03 22:29:59 +0000

    new file:   .gitignore 	new file:   index.html 	new file:   package-lock.json 	modified:   package.json 	new file:   src/Lib/utils.js 	new file:   src/components/Ui/botao.jsx 	new file:   src/components/Ui/botao_tela22.jsx 	new file:   src/components/Ui/card.jsx 	new file:   src/components/Ui/entrada.jsx 	new file:   src/components/Ui/rotulo.jsx 	new file:   src/screens/Moldura/Quadro.jsx 	renamed:    screens/home.js -> src/screens/home.js 	new file:   src/screens/indice.jsx 	new file:   tailwind.config.js 	new file:   tailwind.css 	new file:   vite.config.js

---
+++ src/screens/Moldura/Quadro.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Button } from "../../components/ui/button";
+
+export const Frame = () => {
+  return (
+    <div
+      className="bg-[#4f5a00] grid justify-items-center [align-items:start] w-screen min-h-screen"
+      data-model-id="51:47"
+    >
+      <div className="bg-[#4f5a00] overflow-hidden w-[281px] h-[554px] relative">
+        <div className="w-[284px] h-[350px] -top-7 -left-0.5 bg-[url(https://c.animaapp.com/mf4hrtqcQ1OTcH/img/rectangle-17.svg)] absolute bg-[100%_100%] translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:0ms]">
+          <img
+            className="absolute w-[281px] h-[311px] top-[29px] left-0.5 object-cover translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]"
+            alt="Preview"
+            src="https://c.animaapp.com/mf4hrtqcQ1OTcH/img/preview-1.png"
+          />
+        </div>
+
+        <div className="absolute top-[524px] left-[67px] [font-family:'Inter',Helvetica] font-normal text-white text-[10px] tracking-[0] leading-[normal] whitespace-nowrap translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:800ms]">
+          Todos os Direitos reservados©
+        </div>
+
+        <Button className="w-[196px] h-[35px] top-[366px] left-[38px] bg-[url(https://c.animaapp.com/mf4hrtqcQ1OTcH/img/rectangle-19.svg)] absolute bg-[100%_100%] border-0 rounded-none p-0 hover:opacity-80 transition-opacity translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms] h-auto">
+          <div className="w-[177px] top-[5px] left-3.5 absolute [font-family:'Inter',Helvetica] font-black text-white text-xl tracking-[0] leading-[normal] whitespace-nowrap">
+            Deseja comprar?
+          </div>
+        </Button>
+
+        <Button className="w-[196px] h-[35px] top-[431px] left-[38px] bg-[url(https://c.animaapp.com/mf4hrtqcQ1OTcH/img/rectangle-19.svg)] absolute bg-[100%_100%] border-0 rounded-none p-0 hover:opacity-80 transition-opacity translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:600ms] h-auto">
+          <div className="top-1 left-5 absolute [font-family:'Inter',Helvetica] font-black text-white text-xl tracking-[0] leading-[normal] whitespace-nowrap">
+            Deseja vender?
+          </div>
+        </Button>
+      </div>
+    </div>
+  );
+};
